9835c3b1bc0d832774f17ca62bb5a7fdf8ac156a
SuryaAnything <surya2019npd@gmail.com>
2021-06-17 17:31:17 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+    const increase = document.querySelector("#increase");
+    const decrease = document.querySelector("#decrease");
+    const reset = document.querySelector("#reset");
+    const count = document.querySelector("#count");
+  
+    increase.addEventListener("click", function () {
+      count.innerText = parseInt(count.innerText) + 1;
+    });
+  
+    decrease.addEventListener("click", function () {
+      // logic for decrease
+      if (parseInt(count.innerText) > 0) {
+        count.innerText = parseInt(count.innerText) - 1;
+      }
+    });
+  
+    reset.addEventListener("click", function () {
+      count.innerText = 0;
+    });
+  
+    // setTimeout -->
+    //   setTimeout(() => {
+    //     alert("hey");
+    //   }, 2000);
+  
+    //setInterval
+    //   setInterval(() => {
+    //     alert("hey");
+    //   }, 2000);
+  };
+
+  0) git status
+  1) git Init 
+  2) git remote add origin
+  3) git add . 
+  4) git commit -m "meaningful messsage"
+  5) git push origin master 
